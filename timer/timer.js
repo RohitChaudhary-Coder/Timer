f39d743c1d133f8ea1a992809737f993b38751e4
RohitChaudhary-Coder <chaudharyrohit3572@gmail.com>
2021-02-17 19:19:23 +0530

    set error

--- timer/timer.js
+++ timer/timer.js
@@ -8,7 +8,6 @@ function startTimer(){
     $("#stop-btn").show();
     timerInterval = getTimerInterval();
     timerID = setInterval(UpdateTimerInterval, 1000);  
-    console.log("startTimer")
 }
 // stop timer
 function stopTimer(){
@@ -16,19 +15,18 @@ function stopTimer(){
     $("#stop-btn").hide();
     $(".input-fields").show();
     $("#start-btn").show();
-    console.log("stopTimer")
 }
 // get time hear
 function getTimerInterval(){
     let hours = document.getElementById("Hours").value;
     let minutes = document.getElementById("Minutes").value;
     let seconds = document.getElementById("Seconds").value;
-    console.log((hours * 60 * 60 ) , (minutes * 60) ,seconds*1)
-    return (hours * 60 * 60 ) + (minutes * 60) + seconds*1;
+
+    return (hours * 60 * 60 ) + (minutes * 60) + parseInt(seconds);
 }
 // update timer hear
 function UpdateTimerInterval(){
-    if(timerInterval){
+    if(timerInterval && timerInterval>0){
         $("#inner-container").css({boxShadow: "0px 0px 15px 15px rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"});
         setTimeout(function(){
             $("#inner-container").css({boxShadow: "0px 0px 0px 0px rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"});
@@ -50,10 +48,7 @@ function UpdateTimerInterval(){
         document.getElementById("hour").innerHTML = remain_hours;
         document.getElementById("second").innerHTML = remain_second;
         document.getElementById("minute").innerHTML = remain_miniuts;
-        console.log("no Time out",remain_hours,remain_miniuts,remain_second,remendar)
-
     }else{
-        console.log("Time out")
         $("#time").hide()
         $("#show-time-out").show();
         $(".outer-container").css({"background-color": "red"});  
