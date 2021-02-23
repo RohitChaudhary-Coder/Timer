f3a1fb20e4b018f52f809c52e26620d7e89ad444
Ravi-Kaushish <ravikantsharma2169@gmail.com>
2021-02-23 15:13:34 +0530

    Removed obsolete files

--- learn_js/i.js
+++
@@ -1,9 +0,0 @@
-console.log("harry")
-const mark=[32,54,24,34,44];
-const fruits=['orange','Apple','Pineapple'];
-const mixed =['str',39,[3,5]];
-const arr=new Array(23,234,2343,'Orange')
-console.log(arr)
-console.log(mixed[2][1])
-console.log(arr.length)
-console.log(Array.isArray(""))
--- projects/input_tags.js
+++
@@ -1,5 +0,0 @@
-function get_value(){
-    var value= document.getElementById("age").value;
-    document.getElementById("button");
-    console.log(value);    
-}
